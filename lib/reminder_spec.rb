63bcf67f2fc1ecda78b04ab7b2234332ea15ea96
wimdavies <william.johnston.davies@gmail.com>
2023-04-12 10:41:47 +0100

    worked through testing classes with equality demo

---
+++ lib/reminder_spec.rb
@@ -0,0 +1,10 @@
+require 'reminder'
+
+RSpec.describe Reminder do
+  it "reminds user to perform a task"
+    reminder = Reminder.new("Kay")
+    reminder.remind_me_to("Walk the dog")
+    result = reminder.remind()
+    expect(result).to eq "Walk the dog, Kay!"
+  end
+end
