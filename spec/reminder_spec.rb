902ce27249fcd33306c7b7c5fab83fde9b6e464c
wimdavies <william.johnston.davies@gmail.com>
2023-04-12 10:46:08 +0100

    fixed error with intro spec file

--- spec/reminder_spec.rb
+++ spec/reminder_spec.rb
@@ -1,7 +1,7 @@
 require 'reminder'
 
 RSpec.describe Reminder do
-  it "reminds user to perform a task"
+  it "reminds user to perform a task" do
     reminder = Reminder.new("Kay")
     reminder.remind_me_to("Walk the dog")
     result = reminder.remind()
